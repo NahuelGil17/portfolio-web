341d5cc56bfa68ccc411941f9ee95dc29fb6e28a
Nahuel Gil <nahuel.gil@woow.com.uy>
2022-01-07 15:43:17 -0300

    feat(Root): First Commit
    Adding layout using tailwind

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalComponent } from './landing/pages/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PrincipalComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
